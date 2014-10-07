0444cb91c7b9a7443f15024968d843423fd25802
LucasMazetto <lucas@hlp.com.br>
2014-10-07 11:45:23 -0300

    aJUSTES

--- HLP.Controls.WindowTeste/MainWindow.xaml.cs
+++ HLP.Controls.WindowTeste/MainWindow.xaml.cs
@@ -31,9 +31,9 @@ namespace HLP.Controls.WindowTeste
 
             (this.DataContext as MainWindowDataContext).selectedFruta = 2;
 
-            this.hlpStatus.selectStatus = 1;
+            //this.hlpStatus.selectStatus = 1;
 
-            this.tvMains.BeginLoad();           
+            //this.tvMains.BeginLoad();           
 
 
         }
