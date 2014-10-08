be23502e58ae170db770213d4779aaf3b7737ee0
dramosti <diego@hlp.com.br>
2014-10-08 07:56:54 -0300

    drr

--- HLP.Controls/Component/HlpCheckBox.xaml.cs
+++ HLP.Controls/Component/HlpCheckBox.xaml.cs
@@ -28,10 +28,6 @@ namespace HLP.Controls.Component
             InitializeComponent();        
         }
 
-
-
-
-
         public bool IsChecked
         {
             get { return (bool)GetValue(IsCheckedProperty); }
