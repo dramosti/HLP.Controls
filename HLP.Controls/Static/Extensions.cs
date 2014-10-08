f11b52c92f90976dca68df6dbddc492b9379b330
TakeeshiUtiyama <carlos.santos@hlp.com.br>
2014-10-08 15:58:13 -0300

    Sync

---
+++ HLP.Controls/Static/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLP.Controls.Static
+{
+    public static class Extensions
+    {
+        public static void SetPropertyValue(this object containingObject, string propertyName, object newValue)
+        {
+            if (containingObject != null)
+                containingObject.GetType().InvokeMember(propertyName, BindingFlags.SetProperty, null, containingObject, new object[] { newValue });
+        }
+    }
+}
