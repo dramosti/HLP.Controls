62f5341bcb5970de350d3369ace48c55139e0541
TakeeshiUtiyama <carlos.santos@hlp.com.br>
2014-10-10 15:47:52 -0300

    sync

--- HLP.Controls/Component/CustomIntellisenseColumn.cs
+++ HLP.Controls/Component/CustomIntellisenseColumn.cs
@@ -1,8 +1,11 @@
-﻿using HLP.Controls.Static;
+﻿using HLP.Controls.Base;
+using HLP.Controls.Static;
 using HLP.Controls.ViewModel.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -49,27 +52,127 @@ namespace HLP.Controls.Component
     /// </summary>
     public class CustomIntellisenseColumn : DataGridBoundColumn
     {
-
-        TextBox txt;
-        Popup pup;
-        //<Popup 
-        //       MaxHeight="200" Name="popUp" Placement="Bottom" PlacementTarget="{Binding ElementName=txt}">
-        //    <DataGrid Style="{x:Null}" GridLinesVisibility="None" IsReadOnly="True" 
-        //              SelectedItem="{Binding RelativeSource={RelativeSource Mode=FindAncestor, AncestorType=UserControl}, 
-        //        Path=customViewModel.selectedItem}"
-        //        ItemsSource="{Binding RelativeSource={RelativeSource Mode=FindAncestor, AncestorType=UserControl}, 
-        //        Path=customViewModel.cvs}"></DataGrid>
-        //</Popup>
-
         public CustomIntellisenseColumn()
-        {   
+        {
+            this.pup = new Popup();
+
+            DataGrid dg = new DataGrid();
+            dg.MouseDoubleClick += dg_MouseDoubleClick;
+            dg.PreviewKeyDown += dg_PreviewKeyDown;
+
+            dg.GridLinesVisibility = DataGridGridLinesVisibility.None;
+            dg.IsReadOnly = true;
+
+            dg.SetBinding(dp: DataGrid.SelectedItemProperty, binding: new Binding
+            {
+                Path = new PropertyPath(path: "selectedItem", pathParameters: new object[] { })
+            });
+
+            dg.SetBinding(dp: DataGrid.ItemsSourceProperty, binding: new Binding
+            {
+                Path = new PropertyPath(path: "cvs", pathParameters: new object[] { })
+            });
+
+            this.pup.Child = dg;
+
+            this.customViewModel = new CustomTextBoxIntellisenseViewModel(popUp: pup);
+
+            this.pup.DataContext = this.customViewModel;
         }
 
-        
+        void dg_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.DataGridOwner.Focus();
 
+                this.DataGridOwner.MoveFocus(request: new TraversalRequest(focusNavigationDirection: FocusNavigationDirection.Next));
+
+                this.DataGridOwner.BeginEdit();
+            }
+            else if (e.Key == Key.Enter)
+            {
+                this.SelectItem();
+            }
+        }
+
+        #region Eventos
+
+        void dg_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            this.SelectItem();
+        }
+
+        #endregion
+
+        #region Local Componentes
+
+        private Popup _pup;
+
+        public Popup pup
+        {
+            get { return _pup; }
+            set { _pup = value; }
+        }
+
+        #endregion
+
+        #region Propriedades
+
+        private CustomTextBoxIntellisenseViewModel _customViewModel;
+
+        public CustomTextBoxIntellisenseViewModel customViewModel
+        {
+            get { return _customViewModel; }
+            set { _customViewModel = value; }
+        }
+
+        private string _xNameView;
+
+        public string xNameView
+        {
+            get { return _xNameView; }
+            set
+            {
+                _xNameView = value;
+            }
+        }
+
+        #endregion
+
+        #region Overriden
 
         protected override FrameworkElement GenerateEditingElement(DataGridCell cell, object dataItem)
-        {    
+        {
+            TextBox txt = new TextBox();
+
+            AutoSelectTextBoxAttachedProperty.SetAutoSelect(obj: txt, value: true);
+
+            this.customViewModel.xNameView = this.xNameView;
+
+            this.customViewModel.GetResult();
+
+            txt.DataContext = this.customViewModel;
+
+            txt.SetBinding(dp: TextBox.TextProperty, binding: new Binding
+            {
+                UpdateSourceTrigger = System.Windows.Data.UpdateSourceTrigger.PropertyChanged,
+                Path = new PropertyPath(path: "xTextSearch", pathParameters: new object[] { })
+            });
+
+            Point p = Util.GetBottomPositionComponentInMainWindow(comp: cell, addHeight: 23);
+
+            pup.HorizontalOffset = p.X;
+            pup.VerticalOffset = p.Y;
+
+            pup.IsOpen = true;
+            pup.StaysOpen = true;
+
+            if (cell.Content.GetType() == typeof(TextBlock))
+            {
+                txt.Text = (cell.Content as TextBlock).Text;
+            }
+
             return txt;
         }
 
@@ -77,9 +180,159 @@ namespace HLP.Controls.Component
         {
             TextBlock lbl = new TextBlock();
 
-            lbl.Text = txt.Text;
+            if (cell.Content != null)
+                if (cell.Content.GetType() == typeof(TextBox))
+                {
+                    lbl.Text = (cell.Content as TextBox).Text;
+                }
 
             return lbl;
         }
+
+        protected override object PrepareCellForEdit(FrameworkElement editingElement, RoutedEventArgs editingEventArgs)
+        {
+
+            base.PrepareCellForEdit(editingElement, editingEventArgs);
+
+            (editingElement as TextBox).Focus();
+
+            return editingElement;
+        }
+
+        #endregion
+
+        #region Propriedades de Dependencia
+
+        public int? selectedId
+        {
+            get { return (int?)GetValue(selectedIdProperty); }
+            set
+            {
+                SetValue(selectedIdProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty selectedIdProperty =
+            DependencyProperty.Register("selectedId", typeof(int?), typeof(CustomIntellisenseColumn), new PropertyMetadata(
+                defaultValue: null, propertyChangedCallback: new PropertyChangedCallback(SelectedIdChanged)));
+
+        public static void SelectedIdChanged(DependencyObject d, DependencyPropertyChangedEventArgs args)
+        {
+            if (d != null)
+            {
+                if (args.NewValue == null)
+                {
+                    (d as HlpIntellisense).txt.Text = string.Empty;
+                }
+                else
+                {
+                    DataGridCell _cell = (d as CustomIntellisenseColumn).GetCurrentCell();
+
+                    if (_cell != null)
+                    {
+                        if (_cell.Content.GetType() == typeof(TextBlock))
+                            (_cell.Content as TextBlock).Text = String.Empty;
+                    }
+                    //(d as HlpIntellisense).txt.Text = String.Empty;
+
+                    if (((d as CustomIntellisenseColumn).customViewModel.cvs != null))
+                    {
+                        ((d as CustomIntellisenseColumn).customViewModel.cvs as BindingListCollectionView)
+                            .CustomFilter = String.Format(format: "Id = {0}", arg0: args.NewValue);
+
+                        (d as CustomIntellisenseColumn).customViewModel.cvs.MoveCurrentToFirst();
+
+                        if ((d as CustomIntellisenseColumn).customViewModel.cvs.CurrentItem != null)
+                        {
+                            string xText = String.Empty;
+
+                            foreach (object item in ((d as CustomIntellisenseColumn).customViewModel.cvs.CurrentItem as DataRowView).Row.ItemArray)
+                            {
+                                if (item != null)
+                                    xText += xText == "" ?
+                                        item.ToString() : " - " + item.ToString();
+                            }
+
+                            //(d as HlpIntellisense).txt.Text = xText;
+
+                            if (_cell != null)
+                            {
+                                if (_cell.Content.GetType() == typeof(TextBlock))
+                                    (_cell.Content as TextBlock).Text = xText;
+                            }
+
+                            //if ((d as CustomIntellisenseColumn).DataGridOwner.DataContext != null)
+                            //{
+                            //    PropertyInfo piCurrentModel = (d as CustomIntellisenseColumn).DataGridOwner.DataContext.GetType()
+                            //        .GetProperty(name: "currentModel");
+
+                            //    if (piCurrentModel != null)
+                            //    {
+                            //        Binding b = BindingOperations.GetBinding(target: d,
+                            //            dp: CustomIntellisenseColumn.selectedIdProperty);
+
+                            //        (d as HlpIntellisense).SetModelObjectFromId(objUsedFromReflection: piCurrentModel.GetValue(obj: (d as HlpIntellisense).DataContext),
+                            //            xPath: b.Path.Path);
+                            //    }
+                            //}
+                        }
+                    }
+                }
+            }
+        }
+
+        private DataGridCell GetCurrentCell()
+        {
+            DataGridCell c = null;
+
+            DataGridRow r = this.DataGridOwner.ItemContainerGenerator.ContainerFromItem(
+                item: this.DataGridOwner.CurrentCell.Item) as DataGridRow;
+
+            if (r != null)
+            {
+                c = Util.GetCell(grid: this.DataGridOwner, row: r, column: this.DisplayIndex);
+            }
+
+            return c;
+        }
+
+        public string ucText
+        {
+            get { return (string)GetValue(ucTextProperty); }
+            set { SetValue(ucTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ucText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ucTextProperty =
+            DependencyProperty.Register("ucText", typeof(string), typeof(CustomIntellisenseColumn), new PropertyMetadata(""));
+
+        #endregion
+
+        #region Métodos
+
+        private void SelectItem()
+        {
+            this.pup.IsOpen = false;
+
+            this.DataGridOwner.Focus();
+
+            this.DataGridOwner.BeginEdit();
+
+            if (this.customViewModel.selectedItem != null)
+            {
+                int intValidated = 0;
+                object vValue = this.customViewModel.selectedItem.Row.ItemArray[this.customViewModel.indexIdProperty];
+
+                if (vValue != null)
+                    if (int.TryParse(s: vValue.ToString(),
+                        result: out intValidated))
+                    {
+                        this.selectedId = 0;
+                        this.selectedId = intValidated;
+                    }
+            }
+        }
+
+        #endregion
     }
 }
--- HLP.Controls/Static/Extensions.cs
+++ HLP.Controls/Static/Extensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace HLP.Controls.Static
 {
@@ -13,6 +14,6 @@ namespace HLP.Controls.Static
         {
             if (containingObject != null)
                 containingObject.GetType().InvokeMember(propertyName, BindingFlags.SetProperty, null, containingObject, new object[] { newValue });
-        }
+        }        
     }
 }
